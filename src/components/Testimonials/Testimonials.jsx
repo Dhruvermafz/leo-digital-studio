85c6788482eccbf62450e6793bdc8ec0697646b6
dhruvermafz <vermadhruv09112002@gmail.com>
2023-11-27 09:08:37 +0530

    neox starter

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+       <section class="flat-testimonials">
+                <div class="container-fuild">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="wrap-testi flex">
+                                <div class="box-testi">
+                                    <div class="heading-section2 " data-aos="fade-up" data-aos-duration="1200">
+                                        <h6 class="">Our Testimonials</h6>
+                                        <h2 class="">What Our Clients Say About Solutions</h2>             
+                                    </div>  
+                                    <div class="swiper-container carousel-3 pagi2">                            
+                                        <div class="swiper-wrapper">
+                                            <div class="swiper-slide">
+                                                <div class="box-inner ">  
+                                                    <div class="inner inner-bottom">
+                                                        <h6 class="lh-27 fw-5"> Sit amet consectetur adipiscing estibulum vitae auctor 
+                                                            orci odio. Fusce hendrerit orci ullamcorper enim, cursus. Netus ut nulla venenatis vel imperdiet nunc</h6>
+                                                       <p class="lh-26">Diam augue quis sed cras placerat blandit quisque vivamus. Fames massa odio habitant nisl commodo eu nunc </p>
+                                                    </div>
+                                                    <div class="author-box flex-three">
+                                                        <div class="image">
+                                                            <img src="assets/images/img-box/testimonials.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="content">
+                                                            <h4 class="fw-5">Somalia D. Silva</h4>
+                                                            <p class="text-color-7 fw-5">CEO & Founder</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="swiper-slide">
+                                                <div class="box-inner ">  
+                                                    <div class="inner inner-bottom">
+                                                        <h6 class="lh-27 fw-5"> Sit amet consectetur adipiscing estibulum vitae auctor 
+                                                            orci odio. Fusce hendrerit orci ullamcorper enim, cursus. Netus ut nulla venenatis vel imperdiet nunc</h6>
+                                                       <p class="lh-26">Diam augue quis sed cras placerat blandit quisque vivamus. Fames massa odio habitant nisl commodo eu nunc </p>
+                                                    </div>
+                                                    <div class="author-box flex-three">
+                                                        <div class="image">
+                                                            <img src="assets/images/img-box/testimonials.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="content">
+                                                            <h4 class="fw-5">Somalia D. Silva</h4>
+                                                            <p class="text-color-7 fw-5">CEO & Founder</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="swiper-slide">
+                                                <div class="box-inner ">  
+                                                    <div class="inner inner-bottom">
+                                                        <h6 class="lh-27 fw-5"> Sit amet consectetur adipiscing estibulum vitae auctor 
+                                                            orci odio. Fusce hendrerit orci ullamcorper enim, cursus. Netus ut nulla venenatis vel imperdiet nunc</h6>
+                                                       <p class="lh-26">Diam augue quis sed cras placerat blandit quisque vivamus. Fames massa odio habitant nisl commodo eu nunc </p>
+                                                    </div>
+                                                    <div class="author-box flex-three">
+                                                        <div class="image">
+                                                            <img src="assets/images/img-box/testimonials.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="content">
+                                                            <h4 class="fw-5">Somalia D. Silva</h4>
+                                                            <p class="text-color-7 fw-5">CEO & Founder</p>
+                                                        </div>
+                                                    </div> 
+                                                </div>
+                                            </div>
+                                            <div class="swiper-slide">
+                                                <div class="box-inner ">  
+                                                    <div class="inner inner-bottom">
+                                                        <h6 class="lh-27 fw-5"> Sit amet consectetur adipiscing estibulum vitae auctor 
+                                                            orci odio. Fusce hendrerit orci ullamcorper enim, cursus. Netus ut nulla venenatis vel imperdiet nunc</h6>
+                                                       <p class="lh-26">Diam augue quis sed cras placerat blandit quisque vivamus. Fames massa odio habitant nisl commodo eu nunc </p>
+                                                    </div>
+                                                    <div class="author-box flex-three">
+                                                        <div class="image">
+                                                            <img src="assets/images/img-box/testimonials.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="content">
+                                                            <h4 class="fw-5">Somalia D. Silva</h4>
+                                                            <p class="text-color-7 fw-5">CEO & Founder</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-pagination2 center">  </div> 
+                                    </div>
+                                </div>
+                                <div class="box-images">
+                                    <div class="inner flex" data-aos="fade-up" data-aos-duration="1200">
+                                        <div class="images style">
+                                            <img src="assets/images/img-box/testimonials-1.png" alt=""/>
+                                        </div> 
+                                        <div class="images ">
+                                            <img src="assets/images/img-box/testimonials-2.png" alt=""/>
+                                        </div> 
+                                        <div class="images style">
+                                            <img src="assets/images/img-box/testimonials-3.png" alt=""/>
+                                        </div> 
+                                    </div>
+                                    <div class="inner flex" data-aos="fade-up" data-aos-duration="1300">
+                                        <div class="images style">
+                                            <img src="assets/images/img-box/testimonials-4.png" alt=""/>
+                                        </div> 
+                                        <div class="images ">
+                                            <img src="assets/images/img-box/testimonials-5.png" alt=""/>
+                                        </div> 
+                                        <div class="images style">
+                                            <img src="assets/images/img-box/testimonials-6.png" alt=""/>
+                                        </div> 
+                                    </div>  
+                                    <div class="image2" data-aos="zoom-in" data-aos-duration="1400">
+                                        <img src="assets/images/img-box/testimonials-7.png" alt=""/>
+                                    </div> 
+                                </div>                       
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> 
+    </div>
+  )
+}
+
+export default Testimonials
