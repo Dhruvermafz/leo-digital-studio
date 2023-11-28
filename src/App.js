9c0210715b9be61d66329a7df7a137b013e62300
dhruvermafz <vermadhruv09112002@gmail.com>
2023-11-28 11:47:20 +0530

    fixed header issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./layouts/Home";
-import About from "./layouts/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import "./App.css";
 
 function App() {
